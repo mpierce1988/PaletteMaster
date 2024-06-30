d96363c4a2c8869be9a8ce5a8bd397ff8158333b
michaelpierce <michael.todd.pierce@gmail.com>
2024-06-30 03:12:28 -0300

    Initial Commit

---
+++ src/PaletteMaster/PaletteMaster.Presentation/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PaletteMaster.Presentation;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ src/PaletteMaster/PaletteMaster.Presentation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PaletteMaster.Presentation;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
